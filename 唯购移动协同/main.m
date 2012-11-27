de30013663284d0d37c7382077a6a05d2f593f50
何 泽峰 <HeZeFeng@nanimatoiMac.local>
2012-11-27 13:41:37 +0800

    Initial Commit

---
+++ 唯购移动协同/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  唯购移动协同
+//
+//  Created by 泽峰 何 on 12-11-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
